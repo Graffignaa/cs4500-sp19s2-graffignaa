58a179e19359ade993106b252a741ce5638802e9
graffignaa <graffigna.a@husky.neu.edu>
2019-01-23 20:55:19 -0500

    Create User model class.

---
+++ src/main/java/com/example/cs4500sp19s2graffignaa/model/User.java
@@ -0,0 +1,51 @@
+package com.example.cs4500sp19s2graffignaa.model;
+
+
+public class User {
+	private Integer id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	
+ 	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	// LOL
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+}
+
