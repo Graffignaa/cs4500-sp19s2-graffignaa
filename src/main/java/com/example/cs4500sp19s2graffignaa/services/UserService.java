36ff7d7c07039aacf0f816f6924e4bf8287b57ad
graffignaa <graffigna.a@husky.neu.edu>
2019-01-23 21:04:47 -0500

    Add method to find user by ID using endpoint /api/user/{userId}

--- src/main/java/com/example/cs4500sp19s2graffignaa/services/UserService.java
+++ src/main/java/com/example/cs4500sp19s2graffignaa/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cs4500sp19s2graffignaa.model.User;
@@ -18,5 +19,16 @@ public class UserService {
 	@GetMapping("/api/user")
 	public List<User> findAllUsers() {
 		return users;
+	}
+	
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(
+		@PathVariable("userId") Integer id) {
+		for(User user: users) {
+			if(user.getId() == id) {
+				return user;
+			}
+		}
+		return null;
 	}
 }
